5f7d2e12da143387013706aedd40b224991c8c3a
melp <huzi.master@outlook.com>
2023-02-11 17:31:56 +0800

    add: day11 part1.

--- src/racket/2022/util.rkt
+++ src/racket/2022/util.rkt
@@ -3,23 +3,12 @@
 (provide parse-inputs)
 
 (define parse-inputs
-  (lambda (filepath)
-    (call-with-input-file filepath
-      (lambda (port)
-        (letrec ([elf-reader (lambda (res)
-                               (let ([line (read-line port)])
-                                 (cond
-                                   [(or (equal? line "")
-                                        (eq? line eof)) res]
-                                   [else (elf-reader (cons (string->number line) res))])))]
-                 [elves-reader (lambda (res)
-                                 (let ([elves (elf-reader '())])
-                                   (cond
-                                     [(empty? elves) res]
-                                     [else (elves-reader (cons elves res))])))])
-          (elves-reader '()))))))
-
-(apply + (take (sort (map (lambda (nums) (apply + nums))
-                          (parse-inputs "input1.txt"))
-                     >)
-               3))
+  (lambda (path consume)
+    (call-with-input-file path
+      (lambda (in)
+        (letrec ([loop (lambda (res)
+                         (let ([line (read-line in)])
+                           (cond
+                             [(equal? line eof) res]
+                             [else (loop (cons (consume line) res))])))])
+          (loop '()))))))
