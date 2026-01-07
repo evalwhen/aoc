557c0f07cf6f08aefa82200dd5760cee79737c4d
HuJianFeng <huzi.master@outlook.com>
2026-01-07 23:01:35 +0800

    add ssax-parser.

--- 2019/ssax-s-expression.scm
+++ 2019/ssax-s-expression.scm
@@ -54,3 +54,36 @@
                     (lambda (tok seed)
                       (cons tok seed))
                     '())
+
+
+(define p (open-input-string "((1) 2 3 ((5)) (4))"))
+;; (define p (open-input-string "(1)"))
+
+;; ssax 核心
+(define (make-parser fdown fup fvalue)
+  ;; 此时开头的 ( 必须已经被消耗掉了
+  (define (run-pair parent-seed port)
+    ;; 这个 lp 始终是在构建当前层的元素，当它发现达到当前层的结束标志之后，就向上返回，由 fup 结合当前层和上层的 seed,返回给更上层
+    (let lp ([current-level-seed (fdown parent-seed)])
+        (let ([tok (next-tok port)])
+          (cond
+           [(string? tok) (lp (fvalue tok current-level-seed))]
+           [(eq? 'right-paren tok) (fup current-level-seed parent-seed)]
+           [(eq? 'left-paren tok) (lp (run-pair current-level-seed port))]
+           [else (error 'unkown-token tok)]))))
+  (lambda (p init-seed)
+    (let ([tok (next-tok p)])
+      (if (eq? 'left-paren tok)
+          (run-pair init-seed p)
+          (error 'must-start-by-left-pair)))))
+
+(define s-parser (make-parser
+                  (lambda (seed) '())
+                  (lambda (child-seed parent-seed)
+                    (if (eq? 'empty-seed parent-seed)
+                        (reverse child-seed)
+                        (cons (reverse child-seed) parent-seed)))
+                  (lambda (tok seed)
+                      (cons tok seed))))
+
+(s-parser p 'empty-seed)
