e7a816b0efda094ebde97f4b4525f3ef71c5b2e8
HuJianFeng <huzi.master@outlook.com>
2026-01-07 20:32:55 +0800

    add ssax-s-expression.

--- 2019/ssax-s-expression.scm
+++ 2019/ssax-s-expression.scm
@@ -5,14 +5,13 @@
       (read-char port)
       (let lp ([c (peek-char port)] [acc '()])
         (cond
-         ;; [(eof-object? c) (list->string (reverse acc))]
          [(char=? c #\() (begin (read-char port)
                                 'left-paren)]
-         [(char=? c #\)) (if (> (length acc) 0)
+         [(char=? c #\)) (if (not (null? acc))
                              (list->string (reverse acc))
                              (begin (read-char port)
                                     'right-paren))]
-         [(char=? c #\space) (if (> (length acc) 0)
+         [(char-whitespace? c) (if (> (length acc) 0)
                                  (begin (read-char port)
                                         (list->string (reverse acc)))
                                  (begin (read-char port)
@@ -21,28 +20,28 @@
                  (read-char port)
                  (lp (peek-char port) (cons c acc)))]))))
 
-;; parse-s-expression to scheme list
-(define (parse-s-expression port tok fdown fup fchar seed)
-  (cond
-   [(eof-object? tok) (begin (displayln tok)  seed)]
-   [(string? tok) (fchar tok seed)]
-   [(eq? tok 'left-paren)
-    (let ([new-seed (fdown seed)]
-          [next-tk (next-tok port)])
-      (if (eq? 'right-paren next-tk)
-          seed
-          (let loop ([child-seed (parse-s-expression port next-tk fdown fup fchar new-seed)])
-            (let ([next-tk (next-tok port)])
-              (cond
-               [(eq? 'right-paren next-tk) (fup seed child-seed)]
-               [(eof-object? next-tk) (fup seed child-seed)]
-               [else (loop (parse-s-expression port next-tk fdown fup fchar child-seed))])))))]
-   [(eq? tok 'right-paren) seed]))
+(define (parse-s-expression port fdown fup fvalue seed)
+  (letrec ([parse (lambda (tok seed)
+                    (cond
+                     [(eof-object? tok) seed]
+                     [(string? tok) (fvalue tok seed)]
+                     [(eq? tok 'left-paren)
+                      (let ([new-seed (fdown seed)]
+                            [next-tk (next-tok port)])
+                        (if (eq? 'right-paren next-tk)
+                            seed
+                            (let loop ([child-seed (parse next-tk new-seed)])
+                              (let ([next-tk (next-tok port)])
+                                (cond
+                                 [(eq? 'right-paren next-tk) (fup seed child-seed)]
+                                 [(eof-object? next-tk) (fup seed child-seed)]
+                                 [else (loop (parse next-tk child-seed))])))))]
+                     [else (error 'unkown-token tok)]))])
+    (parse (next-tok port) seed)))
 
-(define p (open-input-string "(1 2 3 (4)"))
+(define p (open-input-string "(1 2 3 (4 (5 6 (7)"))
 
 (parse-s-expression p
-                    (next-tok p)
                     (lambda (seed) '())
                     (lambda (parent-seed child-seed)
                       (if (null? parent-seed)
